27c5c449ad0ecdeff3302ee485d167815a3d10ea
yash-sethia <56500374+yash-sethia@users.noreply.github.com>
2021-08-04 00:48:08 +0530

    Changed Font awesome icons to Material UI

--- client/src/shared/Sidebar.js
+++ client/src/shared/Sidebar.js
@@ -1,9 +1,15 @@
 import React, {useContext} from "react"
 import "./Sidebar.css"
 import { NavLink, Link } from "react-router-dom";
-
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonIcon from '@material-ui/icons/Person';
+import { Icon } from 'semantic-ui-react'
 
 import { UserContext } from '../pages/AuthContext';
+
 
 class Sidebar extends React.Component {
   constructor(props){
@@ -40,7 +46,10 @@ class Sidebar extends React.Component {
         <ul>
           <span onClick={this.onClickSidebarOpen} className="hamburger">
             <li className="hamburger"><a>
-              <span className="icons"><i className="fas fa-bars" aria-hidden="true"></i></span>
+              <span className="icons">
+                {/* <i className="fas fa-bars" aria-hidden="true"></i> */}
+                <MenuIcon />
+              </span>
               </a>
             </li>
           </span>
@@ -48,31 +57,47 @@ class Sidebar extends React.Component {
           <span>
             <li className="sidenav-item">
               <NavLink to="/dashboard">
-                <span className="icons"><i className="fas fa-home" aria-hidden="true"></i></span>
+                <span className="icons">
+                  {/* <i className="fas fa-home" aria-hidden="true"></i> */}
+                  <HomeIcon />
+                </span>
                 <span className="options">SkillBoard</span>
               </NavLink>
             </li>
             <li className="sidenav-item">
               <Link to = "/overall-analytics">
-                <span className="icons"><i className="fas fa-chart-pie" aria-hidden="true"></i></span>
+                <span className="icons">
+                  {/* <i className="fas fa-chart-pie" aria-hidden="true"></i> */}
+                  <EqualizerIcon /> 
+                </span>
                 <span className="options">Analytics</span>
               </Link>
             </li>
+
             <li className="sidenav-item">
               <NavLink to="/Review-more">
-                <span className="icons"><i className="fas fa-search" aria-hidden="true"></i></span>
+                <span className="icons">
+                  {/* <i className="fas fa-search" aria-hidden="true"></i> */}
+                  <SearchIcon />
+                </span>
                 <span className="options">ReviewMore</span>
               </NavLink>
             </li>
             <li className="sidenav-item">
               <NavLink to={`/profile/${this.state.username}`}>
-                <span className="icons"><i className="fas fa-user" aria-hidden="true"></i></span>
+                <span className="icons">
+                  {/* <i className="fas fa-user" aria-hidden="true"></i> */}
+                  <PersonIcon />
+                </span>
                 <span className="options">Profile</span>
               </NavLink>
             </li>
             <li className="sidenav-item">
               <NavLink to = {`/portfolio/${this.state.userId}`} target ="_blank" >
-                <span className="icons"><i className="fas fa-address-book" aria-hidden="true"></i></span>
+                <span className="icons">
+                  {/* <i className="fas fa-address-book" aria-hidden="true"></i> */}
+                  <Icon name='address book' />
+                </span>
                 <span className="options">Portfolio</span>
               </NavLink>
             </li>
