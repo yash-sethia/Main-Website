75ec6604b999c9d93d92b64b39b936dd4040be4c
yash-sethia <56500374+yash-sethia@users.noreply.github.com>
2021-09-12 03:49:14 +0530

    User will not get his article for review

--- routes/reviewArticle.js
+++ routes/reviewArticle.js
@@ -5,12 +5,20 @@ let Article = require('../models/article.model');
 router.route('/:taskId').get((req, res) => {
     Article.find().then(articles => {
         var art = {};
-
-        for(let i = 0; i < articles.length; i++) {
+        var n = articles.length - 1;
+        var i = Math.random() * n;
+        i = parseInt(i);
+        var limit = i == 0? n : i - 1;
+        var found = false;
+        for(; i != limit; i = (i + 1) % n) {
             if(articles[i].articleID.includes(req.params.taskId)) {
                 art = articles[i];
+                found = true;
                 break;
             }
+        }
+        if(found == false) {
+            art = articles[n];
         }
         return(res.status(200).json({articleData : art}));
       })
